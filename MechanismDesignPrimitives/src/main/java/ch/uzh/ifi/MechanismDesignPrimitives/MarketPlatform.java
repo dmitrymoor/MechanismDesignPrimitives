61446f4480f3f12eb943f5983c7fa4872355b718
dmitrymoor <dmitry.moor@yandex.ru>
2017-10-17 12:21:11 +0200

    market platform 1st commit

---
+++ MechanismDesignPrimitives/src/main/java/ch/uzh/ifi/MechanismDesignPrimitives/MarketPlatform.java
@@ -0,0 +1,23 @@
+package ch.uzh.ifi.MechanismDesignPrimitives;
+
+import java.util.List;
+
+/**
+ * 
+ * @author Dmitry Moor
+ *
+ */
+public class MarketPlatform 
+{
+
+	/**
+	 * Constructor.
+	 */
+	public MarketPlatform()
+	{
+		
+	}
+	
+	private List<ParametrizedQuasiLinearAgent> _buyers;
+	private List<AtomicBid> _sellers;
+}
